9a6a43d7e59c644cb37b91cab8673b85df1acfe4
dylanmc1ntee <dylangmcintee@gmail.com>
2025-01-22 17:21:41 -0500

    basic setup

---
+++ assignment-1/CourseCombinations.java
@@ -0,0 +1,15 @@
+// Author: Dylan McIntee
+// Course: CS2
+// Semester: Spring 2025
+
+// Imports
+import java.util.*;
+
+public class CourseCombinations {
+
+    public static void main(String[] args)
+    {
+
+    }
+    
+}
