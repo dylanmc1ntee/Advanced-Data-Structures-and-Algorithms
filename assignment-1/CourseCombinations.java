5272bb6272d7ef96f08f6269b3eef0a858d0e0d8
dylanmc1ntee <dylangmcintee@gmail.com>
2025-01-27 17:37:49 -0500

    input of names

--- assignment-1/CourseCombinations.java
+++ assignment-1/CourseCombinations.java
@@ -3,13 +3,29 @@
 // Semester: Spring 2025
 
 // Imports
-import java.util.*;
+//import java.io.*;
+//import java.util.*;
+import java.util.Scanner;
 
 public class CourseCombinations {
 
     public static void main(String[] args)
     {
+        Scanner newScan = new Scanner(System.in);
+        int numStudents = newScan.nextInt();
+        newScan.nextLine();
 
+        //HashMap<String, String> mainHash = new HashMap<>();
+        String[] students = new String[numStudents];
+
+        for(int i = 0; i < numStudents; i++)
+        {
+            students[i] = newScan.nextLine();
+        }
+
+        
+
+        newScan.close();
     }
     
 }
