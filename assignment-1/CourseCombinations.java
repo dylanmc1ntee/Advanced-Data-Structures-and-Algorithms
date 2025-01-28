ed4b3403f47c48d5cc4f6034af22a082665316f8
dylanmc1ntee <dylangmcintee@gmail.com>
2025-01-27 19:03:37 -0500

    finished without comments

--- assignment-1/CourseCombinations.java
+++ assignment-1/CourseCombinations.java
@@ -4,7 +4,7 @@
 
 // Imports
 //import java.io.*;
-//import java.util.*;
+import java.util.*;
 import java.util.Scanner;
 
 public class CourseCombinations {
@@ -15,15 +15,47 @@ public class CourseCombinations {
         int numStudents = newScan.nextInt();
         newScan.nextLine();
 
-        //HashMap<String, String> mainHash = new HashMap<>();
+        HashMap<String, String> mainHash = new HashMap<>();
         String[] students = new String[numStudents];
+        int totalSame = 0;
 
         for(int i = 0; i < numStudents; i++)
         {
             students[i] = newScan.nextLine();
         }
 
-        
+        String[] keys = new String[numStudents];
+
+        for(int i = 0; i < numStudents; i++)
+        {
+            String[] tempArray = students[i].split(" ");
+            String classes = "";
+            for(int j = 1; j < tempArray.length; j++)
+            {
+                classes += tempArray[j] + " ";
+            }
+
+            keys[i] = tempArray[0];
+            mainHash.put(tempArray[0], classes);
+        }
+
+        for(int i = 0; i < mainHash.size(); i++)
+        {
+            String[] values = mainHash.get(keys[i]).split(" ");
+
+            for(int j = 0; j < values.length; j++)
+            {
+                for(int v = i + 1; v < mainHash.size(); v++)
+                {
+                    if(mainHash.get(keys[v]).contains(values[j]))
+                    {
+                        totalSame++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(totalSame);
 
         newScan.close();
     }
