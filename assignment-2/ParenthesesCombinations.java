4a3cd92baeafab372451c83e4a8b2415ea43b43e
dylanmc1ntee <dylangmcintee@gmail.com>
2025-02-23 00:20:03 -0500

    rando

--- assignment-2/ParenthesesCombinations.java
+++ assignment-2/ParenthesesCombinations.java
@@ -2,12 +2,23 @@ import java.util.List;
 
 public class ParenthesesCombinations{
 
+    int numOpen = 0;
+    int numClose = 0;
+
     public ParenthesesCombinations(){
         // here
     }
 
     public List<String> generateParentheses(int n){
+
+        ArrayList<String> solutions = new ArrayList<String>;
+
+
         
-        return null;
+        return solutions;
+    }
+
+    private List<String> backtrackToFindCombinations(ArrayList<String> solutions, StringBuilder newCombination, int numO, int numC, int n){
+
     }
 }
