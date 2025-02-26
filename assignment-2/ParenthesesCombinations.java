0d25aba4df451f47d5467cc7792c1e265227929a
dylanmc1ntee <dylangmcintee@gmail.com>
2025-02-25 21:54:38 -0500

    add comments

--- assignment-2/ParenthesesCombinations.java
+++ assignment-2/ParenthesesCombinations.java
@@ -1,49 +1,53 @@
+// Author: Dylan McIntee
+// Course: CS2
+// Semester: Spring 2025
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ParenthesesCombinations{
 
-    int numOpen = 0;
-    int numClose = 0;
+    int numOpen = 0; // number of open brackets in stringbuilder
+    int numClose = 0; // number of closed brackets in stringbuilder
 
-    public ParenthesesCombinations(){
+    public ParenthesesCombinations(){ // constructor
         // here
     }
 
     public List<String> generateParentheses(int n){
 
-        ArrayList<String> solutions = new ArrayList<String>();
-        StringBuilder newSolution = new StringBuilder();
+        ArrayList<String> solutions = new ArrayList<String>(); // creates new arraylist to hold solutions
+        StringBuilder newSolution = new StringBuilder(); // creates new stringbuilder to build solutions
 
-        backtrackToFindCombinations(solutions, newSolution, numOpen, numClose, n);
+        backtrackToFindCombinations(solutions, newSolution, numOpen, numClose, n); // passes the arraylist and stringbuilder into the helper
         
-        return solutions;
+        return solutions; // returns arraylist full of all solutions
     }
 
     private void backtrackToFindCombinations(ArrayList<String> solutions, StringBuilder newCombination, int numO, int numC, int n){
 
-        if(numO == n && numC == n){
+        if(numO == n && numC == n){ // if the string is at max length
 
-            String answer = newCombination.toString();
-            solutions.add(answer);
+            String answer = newCombination.toString(); // creates a string of the solution
+            solutions.add(answer); // puts solution into solutions list
 
-            return;
+            return; // get out
         }
 
-        if(numO < n){
-            newCombination.append("(");
+        if(numO < n){ // if we can add an open bracket
+            newCombination.append("("); // adds open bracket to stringbuilder
 
-            backtrackToFindCombinations(solutions, newCombination, numO + 1, numC, n);
+            backtrackToFindCombinations(solutions, newCombination, numO + 1, numC, n); // recurse
 
-            newCombination.deleteCharAt(newCombination.length() - 1);
+            newCombination.deleteCharAt(newCombination.length() - 1); // backtrack portion
         }
 
-        if(numC < numO){
-            newCombination.append(")");
+        if(numC < numO){ // if we have less closed brackets than open
+            newCombination.append(")"); // adds closed bracket to stringbuilder
 
-            backtrackToFindCombinations(solutions, newCombination, numO, numC + 1, n);
+            backtrackToFindCombinations(solutions, newCombination, numO, numC + 1, n); // recurse
 
-            newCombination.deleteCharAt(newCombination.length() - 1);
+            newCombination.deleteCharAt(newCombination.length() - 1); // backtrack portion
         }
     }
 }
