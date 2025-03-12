3194882f15434bd911d0c52379ac8b6bf019c258
dylanmc1ntee <dylangmcintee@gmail.com>
2025-03-12 18:19:05 -0400

    function setup

---
+++ assignment-3/GreedyRobots.java
@@ -0,0 +1,28 @@
+// Author: Dylan McIntee
+// Course: CS2
+// Semester: Spring 2025
+
+public class GreedyRobots {
+    
+    int[] maxTravelRange;
+    int[] distanceFromDispatch;
+    int numDelivered = 0;
+    int numLeftEmpty = 0;
+
+    public GreedyRobots(int numAvailable, int numThatNeedDelivery, String maxTravelRange, String distanceOfBuildings){
+
+    }
+
+    public void readFiles(String maxTravelRange, String distanceOfBuildings){
+
+    }
+
+    public void assignDeliveries(){
+
+    }
+
+    public void displayResults(){
+        
+    }
+
+}
