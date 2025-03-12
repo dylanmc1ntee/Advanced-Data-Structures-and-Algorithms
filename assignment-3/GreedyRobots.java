5ce7335d97b24bd4977d696300433076189f005e
dylanmc1ntee <dylangmcintee@gmail.com>
2025-03-12 18:52:37 -0400

    populate arrays and other variables

--- assignment-3/GreedyRobots.java
+++ assignment-3/GreedyRobots.java
@@ -2,6 +2,10 @@
 // Course: CS2
 // Semester: Spring 2025
 
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 public class GreedyRobots {
     
     int[] maxTravelRange;
@@ -9,11 +13,47 @@ public class GreedyRobots {
     int numDelivered = 0;
     int numLeftEmpty = 0;
 
-    public GreedyRobots(int numAvailable, int numThatNeedDelivery, String maxTravelRange, String distanceOfBuildings){
+    public GreedyRobots(int numAvailable, int numThatNeedDelivery, String file_maxTravelRange, String file_distanceOfBuildings){
+
+        int numRobots = numAvailable;
+        int numBuildings = numThatNeedDelivery;
+        String file_maxTravel = file_maxTravelRange;
+        String file_buildingDistance = file_distanceOfBuildings;
+
+        maxTravelRange = new int[numRobots];
+        distanceFromDispatch = new int[numBuildings];
+
+        readFiles(file_maxTravel, file_buildingDistance);
 
     }
 
-    public void readFiles(String maxTravelRange, String distanceOfBuildings){
+    public void readFiles(String file_maxTravel, String file_buildingDistance){
+
+        Scanner scan1 = new Scanner(file_maxTravel);
+        int i = 0;
+
+        while (scan1.hasNextLine()){
+            
+            String data = scan1.nextLine();
+            maxTravelRange[i] = Integer.parseInt(data);
+            i++;
+            
+        }
+
+        scan1.close();
+
+        Scanner scan2 = new Scanner(file_buildingDistance);
+        i = 0;
+
+        while (scan2.hasNextLine()){
+            
+            String data = scan2.nextLine();
+            distanceFromDispatch[i] = Integer.parseInt(data);
+            i++;
+            
+        }
+
+        scan2.close();
 
     }
 
@@ -22,7 +62,10 @@ public class GreedyRobots {
     }
 
     public void displayResults(){
-        
+
+        System.out.println("Successful Deliveries: " + numDelivered);
+        System.out.println("Unserved Buildings: " + numLeftEmpty);
+
     }
 
 }
