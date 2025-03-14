b97b86dd110e8d5c74268defe638e0616d034a0a
dylanmc1ntee <dylangmcintee@gmail.com>
2025-03-14 19:59:41 -0400

    add comments

--- assignment-3/GreedyRobots.java
+++ assignment-3/GreedyRobots.java
@@ -2,99 +2,102 @@
 // Course: CS2
 // Semester: Spring 2025
 
-import java.util.Scanner; // Import the Scanner class to read text files
-import java.util.Arrays;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.Scanner; // scanner import
+import java.util.Arrays; // arrays import
+import java.io.File; // file import
+import java.io.FileNotFoundException; // error handling import
 
 public class GreedyRobots {
     
-    int[] maxTravelRange;
-    int[] distanceFromDispatch;
-    int numDelivered = 0;
-    int numLeftEmpty = 0;
+    int[] maxTravelRange; // array to hold robots
+    int[] distanceFromDispatch; // array to hold distances
+    int numDelivered = 0; // number of buildings delivered
+    int numLeftEmpty = 0; // number of buildings left empty handed
 
     public GreedyRobots(int numAvailable, int numThatNeedDelivery, String file_maxTravelRange, String file_distanceOfBuildings){
 
-        int numRobots = numAvailable;
-        int numBuildings = numThatNeedDelivery;
-        numLeftEmpty = numThatNeedDelivery;
-        String file_maxTravel = file_maxTravelRange;
-        String file_buildingDistance = file_distanceOfBuildings;
+        int numRobots = numAvailable; // number of robots
+        int numBuildings = numThatNeedDelivery; // number of buildings
+        numLeftEmpty = numThatNeedDelivery; // sets buildings left to deliver to initial number of buildings
+        String file_maxTravel = file_maxTravelRange; // stores file name of robots
+        String file_buildingDistance = file_distanceOfBuildings; // stores file name of buildings
 
-        maxTravelRange = new int[numRobots];
-        distanceFromDispatch = new int[numBuildings];
+        maxTravelRange = new int[numRobots]; // creates array
+        distanceFromDispatch = new int[numBuildings]; // creates array
 
-        readFiles(file_maxTravel, file_buildingDistance);
+        readFiles(file_maxTravel, file_buildingDistance); // pass files into function
 
     }
 
-    public void readFiles(String file_maxTravel, String file_buildingDistance) {
+    public void readFiles(String file_maxTravel, String file_buildingDistance){
 
         try {
 
-            // Corrected file reading
-            Scanner scan1 = new Scanner(new File(file_maxTravel)); // Open file properly
+            Scanner scan1 = new Scanner(new File(file_maxTravel)); // opens file in scanner
             int i = 0;
-            while (scan1.hasNextInt()) { // Read integers correctly
 
-                maxTravelRange[i++] = scan1.nextInt();
+            while (scan1.hasNextInt()){ // while not at the end of file
+
+                maxTravelRange[i] = scan1.nextInt(); // populate array
+                i++;
 
             }
 
-            scan1.close();
+            scan1.close(); // close scanner
 
-            Scanner scan2 = new Scanner(new File(file_buildingDistance)); // Open file properly
+            Scanner scan2 = new Scanner(new File(file_buildingDistance)); // opens file in scanner
             i = 0;
 
-            while (scan2.hasNextInt()) { // Read integers correctly
+            while (scan2.hasNextInt()) { // while not at the end of file
 
-                distanceFromDispatch[i++] = scan2.nextInt();
+                distanceFromDispatch[i] = scan2.nextInt(); // populate array
+                i++;
 
             }
 
-            scan2.close();
+            scan2.close(); // close scanner
 
-            Arrays.sort(distanceFromDispatch);
-            reverseList(maxTravelRange);
+            Arrays.sort(distanceFromDispatch); // sort building array from least to greatest
+            reverseList(maxTravelRange); // pass array into reverse sort function
 
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e){ // catch error
 
-            System.out.println("Error: File not found - " + e.getMessage());
-            e.printStackTrace();
-            
+            e.printStackTrace(); // print errors
+
         }
 
     }
 
     private void reverseList(int[] arr){
 
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = arr[i] * -1;
+        for(int i = 0; i < arr.length; i++){ // for the length of the array
+            arr[i] = arr[i] * -1; // multiply all by -1 so the greatest number becomes the least
         }
 
-        Arrays.sort(maxTravelRange);
+        Arrays.sort(maxTravelRange); // sort from least to greatest
 
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = arr[i] * -1;
+        for(int i = 0; i < arr.length; i++){ // for the length of the array
+            arr[i] = arr[i] * -1; // multiply by -1 to get origional array back
         }
 
     }
 
     public void assignDeliveries(){
 
-        int buildingsDelivered = 0;
+        int buildingsDelivered = 0; // buildings that have been delivered in this run
 
+        // for every robot && we have not been to every single building
         for (int i = 0; i < maxTravelRange.length && buildingsDelivered < distanceFromDispatch.length; i++){
 
-            int rangeLeft = maxTravelRange[i];
+            int rangeLeft = maxTravelRange[i]; // amount of gas the current robot has
 
+            // while we have not been to every building && we have enough gas to go to the next one
             while(buildingsDelivered < distanceFromDispatch.length && rangeLeft >= distanceFromDispatch[buildingsDelivered]){
 
-                rangeLeft -= distanceFromDispatch[buildingsDelivered];
-                numDelivered++;
-                buildingsDelivered++;
-                numLeftEmpty--;
+                rangeLeft -= distanceFromDispatch[buildingsDelivered]; // subtract the current fuel
+                numDelivered++; // add 1 to the delivered counter
+                buildingsDelivered++; // add 1 to the counter for this run
+                numLeftEmpty--; // subtract 1 from the buildings left empty
 
             }
         }
@@ -103,8 +106,8 @@ public class GreedyRobots {
 
     public void displayResults(){
 
-        System.out.println("Successful Deliveries: " + numDelivered);
-        System.out.println("Unserved Buildings: " + numLeftEmpty);
+        System.out.println("Successful Deliveries: " + numDelivered); // print succesful deliveries
+        System.out.println("Unserved Buildings: " + numLeftEmpty); // print buildings left empty
 
     }
 
